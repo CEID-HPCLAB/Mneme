d603e1f4ed327cf8c5af6c9760baf799c4236714
DmMeta <dimetaxakis@gmail.com>
2026-03-10 13:03:11 +0200

    Update path desc

--- examples/scripts/run.sh
+++ examples/scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 main.py --training_file "/home/hpclab01/Projects/tabular_datasets/train_data_120M.csv" --target_label_name "y0"\
-                -nw 64 -nb 600 -niow 2 -nr 17978709 -bcache "/home/hpclab01/Projects/tabular_datasets/120M_offsets_600bl.dat"
+python3 main.py --training_file "/path/to/dataset.csv" --target_label_name "y0"\
+                -nw 64 -nb 600 -niow 2 -nr 17978709 -bcache "/path/to/offsets_file.dat"
